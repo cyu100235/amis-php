263247817738fca1cb2f117c5cee57a74d1d0f4f
楚羽幽 <958416459@qq.com>
2025-05-29 14:40:43 +0800

    增加示例

---
+++ demo/table/tabs.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 积木云渲染器
+ *
+ * @package  XbCode
+ * @author   楚羽幽 <958416459@qq.com>
+ * @version  1.0
+ * @license  Apache License 2.0
+ * @link     http://www.xbcode.net
+ * @document http://doc.xbcode.net
+ */
+
+// 选项卡表格，待完善
